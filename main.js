fe7fd6c2f3b0be1e75cabb94061f1791dca368dd
yosief11 <109878966+yosief11@users.noreply.github.com>
2022-10-19 20:52:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,77 @@
+let color = "black";
+let click = false;
+
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(16);
+
+    document.querySelector("body").addEventListener("click", function(e){
+        if(e.target.tagName != "BUTTON"){
+            click = !click;
+            let draw = document.querySelector("#draw");
+            if(click){
+                draw.innerHTML = "You're now able to Draw!";
+            }
+            else{
+                draw.innerHTML = "In order to draw, click the mouse!"
+            }
+        }
+    })
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    })
+})
+
+function createBoard(size){
+    let board = document.querySelector(".board");
+
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`; 
+
+    let numDivs = size * size;
+
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        div.addEventListener("mouseover", colorDiv)
+        board.insertAdjacentElement("beforeend", div);
+    }
+
+}
+
+function getSize(){
+    let input = prompt("What will be the size of the board?");
+    let message = document.querySelector("#message");
+    if(input == ""){
+        message.innerHTML = "Please provide a number";
+    }
+    else if(input < 0 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100"
+    }
+    else{
+        message.innerHTML = "You can now play!"
+        return input;
+    }
+}
+
+function colorDiv(){
+    if(click){
+        if(color == "random"){
+            this.style.backgroundColor = 
+            `rgb(${Math.floor(Math.random() * 256)},`
+        }
+        else{
+            this.style.backgroundColor = 'black'
+        }
+    }
+}
+
+function setColor(colorChoice){
+    color = colorChoice;
+}
+
+function resetBoard(){
+    let divs = document.querySelectorAll("div");
+    divs.forEach((div) => div.style.backgroundColor = "white");
+}
